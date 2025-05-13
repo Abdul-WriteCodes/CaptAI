42c1d9df52a9f2cef66de63e35859a9e8d993b8f
Abdul <Abdulwrite77@gmail.com>
2025-05-13 14:15:12 +0100

    Modified model loading in the applicaion

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Created on Thu May  1 01:36:28 2025
 import re
 import streamlit as st
 import joblib
+from sklearn.linear_model import SGDClassifier
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
