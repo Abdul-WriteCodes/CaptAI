4a8d8479746e6b15479c235ed75f6c697d4b927b
Abdul <Abdulwrite77@gmail.com>
2025-12-26 15:17:08 +0100

    updated hyperlink

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ st.markdown("---")
 st.markdown("""
    <div style="text-align: center; font-size: 0.85em; color: gray; line-height: 1.6em;">
     <strong>CaptAI™</strong>: Designed and Developed at <strong>AWC Labs</strong><br>
-    🔬Learn more about Developer: <a href="https://abdul-writecodes.github.io/abprt/index.html" target="_blank">Abdul</a><br>
+    🔬Learn more about Developer: <a href="https://abdul-writecodes.github.io/portfolio/" target="_blank">Abdul</a><br>
     We appreciate voluntary support for this project via 
     <a href="https://www.buymeacoffee.com/abdul_writecodes" target="_blank" style="color: #ff5f1f; font-weight: bold;">☕BuyMeACoffee</a> 
     or 
